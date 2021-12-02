97987e9222069af5890887ad6e7e92a138ccd77b
Mateu Saiós <mateu1502@gmail.com>
2021-12-02 11:02:40 +0100

    Wander and enclosure

--- Characters/NonPlayableCharacter.gd
+++ Characters/NonPlayableCharacter.gd
@@ -1,5 +1,11 @@
 extends KinematicBody2D
 
+const WANDER_CIRCLE_RADIUS: int = 8
+const WANDER_RANDOMNESS: float = 0.2
+var wander_angle: float = 0
+export(bool) var wandering: bool = true
+export(Rect2) var enclosure_zone: Rect2 = Rect2(16, 16, 480, 256)
+
 var max_speed: int = 70
 var max_steering: float = 2.5
 var avoid_force: int = 1000
@@ -22,11 +28,16 @@ func _process(_delta: float) -> void:
 func _physics_process(_delta: float) -> void:
 	var steering: Vector2 = Vector2.ZERO
 	
-	var vector_to_target: Vector2 = get_global_mouse_position() - position
-	if vector_to_target.length() > arrival_zone_radius:
-		steering += seek_steering(vector_to_target)
+	if wandering:
+		steering += enclosure_steering()
+		steering += wander_steering()
 	else:
-		steering += arrival_steering(vector_to_target)
+		var vector_to_target: Vector2 = get_global_mouse_position() - position
+		if vector_to_target.length() > arrival_zone_radius:
+			steering += seek_steering(vector_to_target)
+		else:
+			steering += arrival_steering(vector_to_target)
+			
 	steering += avoid_obstacles_steering()
 	steering = steering.clamped(max_steering)
 	
@@ -59,3 +70,32 @@ func avoid_obstacles_steering() -> Vector2:
 			return (position + velocity - obstacle.position).normalized() * avoid_force
 			
 	return Vector2.ZERO
+	
+	
+func enclosure_steering() -> Vector2:
+	var desired_velocity: Vector2 = Vector2.ZERO
+	
+	if position.x < enclosure_zone.position.x:
+		desired_velocity.x += 1
+	elif position.x > enclosure_zone.position.x + enclosure_zone.size.x:
+		desired_velocity.x -= 1
+	if position.y < enclosure_zone.position.y:
+		desired_velocity.y += 1
+	elif position.y > enclosure_zone.position.y + enclosure_zone.size.y:
+		desired_velocity.y -= 1
+		
+	desired_velocity = desired_velocity.normalized() * max_speed
+	if desired_velocity != Vector2.ZERO:
+		wander_angle = desired_velocity.angle()
+		return desired_velocity - velocity
+	else:
+		return Vector2.ZERO
+	
+	
+func wander_steering() -> Vector2:
+	wander_angle = rand_range(wander_angle - WANDER_RANDOMNESS, wander_angle + WANDER_RANDOMNESS)
+	
+	var vector_to_cicle: Vector2 = velocity.normalized() * max_speed
+	var desired_velocity: Vector2 = vector_to_cicle + Vector2(WANDER_CIRCLE_RADIUS, 0).rotated(wander_angle)
+	
+	return desired_velocity - velocity
--- Game.gd
+++ Game.gd
@@ -1,6 +1,12 @@
 extends YSort
 
 
+func _init() -> void:
+	randomize()
+
+
 func _input(_event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().quit()
+	if Input.is_key_pressed(KEY_P):
+		get_tree().paused = true
